41004568a29a093088a6195b7045429ce58542a2
helolah <jauheloisa@gmail.com>
2021-01-29 10:24:06 -0300

    projeto criado

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import db from '../db.json'
+
+// LAYOUT DA PÁGINA
+export const BackgroundImage = styled.div`
+  background-image: url( ${ db.bg } );
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
+// ESTRUTURA DA PÁGINA
+export default function Home() {
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        HELLO WORLD!!!
+      </QuizContainer>
+    </BackgroundImage>
+  )
+}
